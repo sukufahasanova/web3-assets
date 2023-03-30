bd7d29f800832b7472549603bbbe4e517251318d
javidselimov12345 <javidzs@code.edu.az>
2023-03-30 12:39:17 +0400

    added chnages

---
+++ js/assets/js/main.js
@@ -0,0 +1,70 @@
+
+const name = "YaKudza - Zaqatala";
+
+console.log(name);
+
+
+
+
+
+
+
+
+// let a = 7;
+
+// let  a = 6; // bele olmaz
+
+// console.log(a)
+
+// const b = 5; // sabite ikinci deyeri menimsetmek olmaz
+
+// b = 8;
+
+// console.log(
+//     b
+// )
+
+
+
+// let heading = document.querySelector('h1');
+// let btn = document.querySelector('button');
+
+// btn.addEventListener('click', () => {
+//     heading.innerText = "AAAAAAA Men Neynedim!!";
+//     heading.style.backgroundColor = "blue";
+//     heading.style.color = "white";
+
+// })
+
+
+// console.log(heading)
+
+
+
+// function chargeAccount(value) {
+//     if (!account) {
+//         account = 0;
+//     }
+//     account += value;
+// }
+
+// function withdraw(value) {
+//     account -= value;
+// }
+
+// chargeAccount(1000);
+// chargeAccount(1000);
+// withdraw(1500);
+// withdraw(300);
+
+// console.log(account);
+
+
+// function _(){
+//     console.log('Yakudza')
+// }
+
+// _();
+
+
+
