4a2ea591faeabb7d50b23c331a23a488027a0d06
javidselimov12345 <javidzs@code.edu.az>
2023-04-07 10:10:08 +0400

    added value task

--- js/assets/js/progrTec.js
+++ js/assets/js/progrTec.js
@@ -1,8 +1,121 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// value dəyişəni üçün müxtəlif müqayisə operatorları ilə 5 ifadə yazın, hansıki əməliyyat nəticəsi value = 10 üçün true olacaq və
+// value = 20 üçün false.
+
+
+// let value = 10;
+
+// console.log(value<20);
+// console.log(value<=15);
+// console.log(value!=20);
+// console.log(value=="10");
+// console.log(value===10);
+
+
+
+
+
+
+
+
+
+
+
+// let value = 10;
+// console.log(value > 0);
+// console.log(value === 10);
+// value = 20;
+// console.log(value!==20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(25 === "25");
+
+// console.log(14 !== "14");
+
+// console.log(0 === "");
+
+
+
+
+
+
+
+
+
+
+
+
+// let a;
+
+// // a = 4; // assignement;
+// a = null; // assignement;
+
+
+// console.log(a==0)
+
+
+
+// console.log(a == 6); // false
+// console.log(a == 4); // true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // İstifadəçidən soyad, ad və ata adını istəyən, soyadı və baş hərfləri konsola çıxaran proqram tərtib edin.
 // Məsələn, «İvanov Pyotr Sergeevich» — «İvanov P.S.».
 
-let fullName = prompt("Zehemt olmasa adinizi «İvanov Pyotr Sergeevich» formasinda daxil edin");
+// let fullName = prompt("Zehemt olmasa adinizi «İvanov Pyotr Sergeevich» formasinda daxil edin");
+
+// let collection = fullName.split(' ');
+
+// console.log(collection);
+
+
+// Second way
+// console.log(`${collection[0]} ${collection[1][0]}.${collection[2][0]}.`);
 
 
 
