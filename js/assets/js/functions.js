dd166be36165eadc99fe8a6a8552c6a60c8a36e5
javidselimov12345 <javidzs@code.edu.az>
2023-04-10 10:05:09 +0400

    added chnages

--- js/assets/js/functions.js
+++ js/assets/js/functions.js
@@ -1,18 +1,40 @@
+let balance = 2000;
 
-let balance = 1200;
-
-function log() {
-    console.log("Balans deyishdi");
-    console.log(balance);
+function log(balance) {
+  console.log(`Sizin balansiniz ${balance} teshkil edir`);
 }
 
+function withdraw(amount) {
+    console.log(`siz balansdan ${amount} qeder pul cixarmaq isteyirsiz`)
+    console.log(`*************************************`)
+  if (balance < amount) {
+      console.log("Balansinizda kifayet qeder mebleg yoxdur");
+      
+  } else {
+    balance = balance - amount;
+  }
 
-balance -= 200;
-log();
+    log(balance);
+     console.log(`*************************************`);
+}
 
-balance -= 400;
-log();
+withdraw(200);
+withdraw(400);
+withdraw(100);
+withdraw(2000);
 
+// let balance = 1200;
 
-balance -= 20;
-log();
+// function log() {
+//     console.log("Balans deyishdi");
+//     console.log(balance);
+// }
+
+// balance -= 200;
+// log();
+
+// balance -= 400;
+// log();
+
+// balance -= 20;
+// log();
--- js/assets/lessons/lesson-8.js
+++ js/assets/lessons/lesson-8.js
@@ -2,8 +2,37 @@
  * logGreeting() adında funksiya tərtib edin hansıki konsola «[Adınız] is a JS developer» mesajı çıxarsın.
  */
 
-function logGreeting() {
-    console.log('Javid is a JS developer');
+// function logGreeting() {
+//     console.log('Javid is a JS developer');
+// }
+
+// logGreeting();
+
+// *************************************************
+
+/**İstifadəçi adını parametr kimi qəbul edən funksiya yazın.
+İstifadəçi adını parametr kimi qəbul edən və konsola «[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin.
+Funksiyanı 3 müxtəlif ad üçün çağırın. */
+
+function isActiveUser(name) {
+    console.log(`${name} is active now`);
 }
 
-logGreeting();
+isActiveUser('Javid');
+console.log('*********');
+
+isActiveUser("Nemet");
+console.log("*********");
+
+isActiveUser("Zulfugar");
+console.log("*********");
+
+
+
+
+
+
+
+
+
+
