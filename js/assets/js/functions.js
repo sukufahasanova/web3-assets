24a57dc90fa4f41a3c2e5bc82cd0afcfb7ab7243
javidselimov12345 <javidzs@code.edu.az>
2023-04-10 10:24:14 +0400

    added chnages

--- js/assets/js/functions.js
+++ js/assets/js/functions.js
@@ -1,27 +1,59 @@
-let balance = 2000;
+// function isletterOfFive(word) {
+//     if (typeof word === 'number') {
+//         console.log("zehmet olmasa setir tipi daxil edin");
+//     }else if (word.length !== 5) {
+//          console.log('Duzgun uzunluqda deyil')
+//     } else {
+//         console.log('Soz 5 herifden ibaretdir')
+//      }
+// }
 
-function log(balance) {
-  console.log(`Sizin balansiniz ${balance} teshkil edir`);
-}
+// isletterOfFive(65555)
+ 
 
-function withdraw(amount) {
-    console.log(`siz balansdan ${amount} qeder pul cixarmaq isteyirsiz`)
-    console.log(`*************************************`)
-  if (balance < amount) {
-      console.log("Balansinizda kifayet qeder mebleg yoxdur");
-      
-  } else {
-    balance = balance - amount;
-  }
 
-    log(balance);
-     console.log(`*************************************`);
-}
 
-withdraw(200);
-withdraw(400);
-withdraw(100);
-withdraw(2000);
+
+
+
+
+
+// function difference(a, b) {
+//   console.log(a - b);
+// }
+
+// difference(7, 5);
+// // 2
+// difference(5, 7);
+// // -2
+
+
+
+
+// let balance = 2000;
+
+// function log(balance) {
+//   console.log(`Sizin balansiniz ${balance} teshkil edir`);
+// }
+
+// function withdraw(amount) {
+//     console.log(`siz balansdan ${amount} qeder pul cixarmaq isteyirsiz`)
+//     console.log(`*************************************`)
+//   if (balance < amount) {
+//       console.log("Balansinizda kifayet qeder mebleg yoxdur");
+
+//   } else {
+//     balance = balance - amount;
+//   }
+
+//     log(balance);
+//      console.log(`*************************************`);
+// }
+
+// withdraw(200);
+// withdraw(400);
+// withdraw(100);
+// withdraw(2000);
 
 // let balance = 1200;
 
--- js/assets/lessons/lesson-8.js
+++ js/assets/lessons/lesson-8.js
@@ -14,22 +14,32 @@
 İstifadəçi adını parametr kimi qəbul edən və konsola «[İstifadəçi adı] is active now» mesajı çıxaran funksiya tərtib edin.
 Funksiyanı 3 müxtəlif ad üçün çağırın. */
 
-function isActiveUser(name) {
-    console.log(`${name} is active now`);
+// function isActiveUser(name) {
+//     console.log(`${name} is active now`);
+// }
+
+// isActiveUser('Javid');
+// console.log('*********');
+
+// isActiveUser("Nemet");
+// console.log("*********");
+
+// isActiveUser("Zulfugar");
+// console.log("*********");
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/**
+Üç ədədi parametr kimi qəbul edən funksiya yazın. Funksiya onlardan ən böyüyünü konsola çıxarmalıdır. */
+
+
+function maxNum(a,b,c) {
+    console.log(Math.max(a,b,c))
 }
 
-isActiveUser('Javid');
-console.log('*********');
-
-isActiveUser("Nemet");
-console.log("*********");
-
-isActiveUser("Zulfugar");
-console.log("*********");
-
-
-
-
+maxNum(5, 8, 9);
+maxNum(10, 8, 9);
+maxNum(5, 20, 3);
 
 
 
