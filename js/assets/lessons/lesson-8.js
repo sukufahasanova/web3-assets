ae19f687a2360a44267ff50ee4220e6a9bc1e394
javidselimov12345 <javidzs@code.edu.az>
2023-04-10 09:17:54 +0400

    added lesson-js

---
+++ js/assets/lessons/lesson-8.js
@@ -0,0 +1,9 @@
+/**
+ * logGreeting() adında funksiya tərtib edin hansıki konsola «[Adınız] is a JS developer» mesajı çıxarsın.
+ */
+
+function logGreeting() {
+    console.log('Javid is a JS developer');
+}
+
+logGreeting();
