80c2f44d60bb68a16c56d3f7ab11c419b7096c3e
javidselimov12345 <javidzs@code.edu.az>
2023-04-10 09:22:24 +0400

    function lesson

---
+++ js/assets/js/functions.js
@@ -0,0 +1,18 @@
+
+let balance = 1200;
+
+function log() {
+    console.log("Balans deyishdi");
+    console.log(balance);
+}
+
+
+balance -= 200;
+log();
+
+balance -= 400;
+log();
+
+
+balance -= 20;
+log();
