fc26f635862fcbf34579841c24a756f0ce39eaf4
javidselimov12345 <javidzs@code.edu.az>
2023-04-10 12:12:01 +0400

    added the isLucky

--- js/assets/lessons/lesson-9.js
+++ js/assets/lessons/lesson-9.js
@@ -1,6 +1,6 @@
 /**
  * Parametr kimi iki ədəd qəbul edən və onlardan ən kiçiyini qaytaran min() funksiyası tərtib edin.
- * 
+ *
  */
 
 // function min(a,b) {
@@ -16,20 +16,36 @@
 Məsələn, calc(4, 6, '+');  //  10 qaytaracaq
  */
 
-function calc(numOne, numTwo, operator) {
-    // switch (operator) {
-    //     case '+': return numOne + numTwo
-    //     case '-': return numOne - numTwo
-    //     case '*': return numOne * numTwo
-    //     case '/': return numOne / numTwo
+// function calc(numOne, numTwo, operator) {
+//     // switch (operator) {
+//     //     case '+': return numOne + numTwo
+//     //     case '-': return numOne - numTwo
+//     //     case '*': return numOne * numTwo
+//     //     case '/': return numOne / numTwo
 
-    // }
-    if (operator === "+")  return numOne + numTwo;
-    if (operator === "-")  return numOne - numTwo;
-    if (operator === "*")  return numOne * numTwo;
-    if (operator === "/")  return numOne / numTwo;
-    
+//     // }
+//     if (operator === "+")  return numOne + numTwo;
+//     if (operator === "-")  return numOne - numTwo;
+//     if (operator === "*")  return numOne * numTwo;
+//     if (operator === "/")  return numOne / numTwo;
+
+// }
+
+// console.log(calc(4, 6, "+"));
+
+/**
+ * Altı rəqəmli ədəd qəbul edən və ilk üç rəqəminin cəminin ikinci üç rəqəminin cəminə bərabərliyin yoxlayan isLucky funksiyasını yazın.
+Məsələn, isLucky(234801);  // true qaytaracaq
+ */
+
+
+function isLucky(number) {
+    number = String(number);
+    return (
+      +number[0] + +number[1] + +number[2] ===
+      +number[3] + +number[4] + +number[5]
+    );
+  
 }
 
-console.log(calc(4, 6, "+"));
-
+isLucky(234801);
