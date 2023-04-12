0edf8fee646794631683576739ab8c4dbbfae018
javidselimov12345 <javidzs@code.edu.az>
2023-04-12 12:02:29 +0400

    added chnages

---
+++ js/assets/js/massiv.js
@@ -0,0 +1,41 @@
+// idOfSpiderMan1 = 0;
+// pagesOfSpiderMan1 = 32;
+// idOfSpiderMan2 = 1;
+// pagesOfSpiderMan2 = 31;
+// idOfSpiderMan3 = 2;
+// pagesOfSpiderMan3 = 34;
+// idOfSpiderMan4 = 3;
+// pagesOfSpiderMan4 = 36;
+// idOfSpiderMan5 = 4;
+// pagesOfSpiderMan5 = 31;
+
+
+// let info = [
+
+//     [0,32],
+//     [1,31],
+// ]
+
+
+
+
+
+
+
+// ****************************************************************************
+// ****************************************************************************
+// ****************************************************************************
+
+
+
+// function ioKio() {
+//     console.log("IoKIo")
+// }
+
+// // let a ="wefwefwef" ["w","w","f"]
+
+// // let animalList = ["Tom", "Spike", "qefwef", 6, true, ['name','Fio'],];
+// let dummYlist = ["Tom", "Spike", "qefwef", 6, true, ['name', 'Fio'], ioKio];
+// console.log(dummYlist.length);
+
+// dummYlist[dummYlist.length-1]()
---
+++ js/assets/lessons/lesson-12.js
@@ -0,0 +1,10 @@
+/**
+ * Massiv elementinin dəyərini dəyişin.
+Üç addan ibarət massiv yaradın. Massivin ikinci elementinin dəyərini “Classified” ilə əvəz edin. Massivi konsola çıxarın.
+ */
+
+// const characters = ["name0", "namee", "name2"];
+
+// characters[1] = "Classified";
+
+// console.log(characters[1]);
