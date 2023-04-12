66682ae58e9873df865c2cb0abf734915a2f0769
javidselimov12345 <javidzs@code.edu.az>
2023-04-12 12:25:28 +0400

    added changes

--- js/assets/lessons/lesson-12.js
+++ js/assets/lessons/lesson-12.js
@@ -8,3 +8,41 @@
 // characters[1] = "Classified";
 
 // console.log(characters[1]);
+
+
+
+
+// ********************************************************************
+// ********************************************************************
+// ********************************************************************
+
+/**
+ * "Episode 4: New Hope" kimi mesajları ardıcıl olaraq konsola çıxarın.
+Massiv verilib. "Episode 4: New Hope" kimi mesajları ardıcıl olaraq konsola çıxarın. Massiv elementlərin düzün
+ */
+
+// let arr = ["New Hope", "The Empire Strikes Back", "Return of the Jdi"];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`Episode ${i + 4}: ${arr[i]}`);
+// }
+
+
+/**
+ * 
+ * İstifadəçidən beş ədəd elementi daxil etməyi tələb edən, onları massilə yazan və ən kiçik ədədi konsola çıxaran proqram yazın.
+ */
+// let a = prompt("Vergulle ayrilan 5 eded daxil et").split(",");
+// while (a.length !== 5) {
+//   a = prompt("Vergulle ayrilan 5 eded daxil et").split(",");
+// }
+
+// let min = +a[0];
+
+// for (let i = 1; i < a.length; i++) {
+//   if (+a[i] < min) {
+//     min = +a[i];
+//   }
+// }
+
+// console.log(min);
