95527ea15dcc25ef54cd654ec316f09ac461cbc1
javidselimov12345 <javidzs@code.edu.az>
2023-04-12 10:05:40 +0400

    added the second lesson11

--- js/assets/lessons/lesson-11.js
+++ js/assets/lessons/lesson-11.js
@@ -3,10 +3,21 @@
 Sayğaclı dövrün köməyi ilə 10-a qədər bütün ədədlərin hasilin tapan proqram tərtib edin. 
 Alınan dəyəri konsola çıxarın.
  */
-let sumOfMultiply = 1;
+// let sumOfMultiply = 1;
 
-for (let i = 1; i <= 10; i++) {
-  sumOfMultiply *= i;
+// for (let i = 1; i <= 10; i++) {
+//   sumOfMultiply *= i;
+// }
+
+// console.log(`The hasil of numbers up to 10 is ${sumOfMultiply}`);
+
+
+/**
+ * İstifadəçidən mətn daxil etməsini tələb edən və mətn elementlərini əks qaydada düzən proqram tərtib edin.
+ */
+
+let num = "Ariel";
+
+for (let i = num.length - 1; i >= 0; i--){
+    console.log(num[i])
 }
-
-console.log(`The hasil of numbers up to 10 is ${sumOfMultiply}`);
