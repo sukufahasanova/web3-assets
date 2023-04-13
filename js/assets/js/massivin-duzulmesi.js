f59a93b7417edf0daaea6f23c6db405bd61de99b
javidselimov12345 <javidzs@code.edu.az>
2023-04-13 11:02:51 +0400

    added

--- js/assets/js/massivin-duzulmesi.js
+++ js/assets/js/massivin-duzulmesi.js
@@ -1,16 +1,53 @@
-let arrayOfstudents = [];
 
-arrayOfstudents.push("Zulfugar");
-arrayOfstudents.push("Serxan");
-arrayOfstudents.push("Emil");
+const retro = ['music', 'cinema', 'theatre', 'Ballet'];
 
-console.log(arrayOfstudents.length);
+const [aba, baba, caba, xaba] = retro;
 
-let yaxciOglan = arrayOfstudents.pop();
-let yaxciOglan2 = arrayOfstudents.shift();
-console.log(yaxciOglan2,"hehhehe");
-arrayOfstudents.unshift("Deputat");
-console.log(arrayOfstudents);
+const [melody,film,...other] = retro
+
+console.log(melody);
+console.log(other);
+
+// console.log(aba);
+// console.log(baba);
+// console.log(caba);
+// console.log(xaba);
+
+
+
+
+
+
+
+
+// const dummArray = ['Holly Dolly', 'Tom Jerry', 'Frozen', 'Rango'];
+
+// let dummArrayString = dummArray.join("-");
+
+// console.log(dummArrayString);
+// console.log(dummArray);
+
+// dummArrayString = dummArrayString.split('-').join('====');
+
+// console.log(dummArrayString);
+
+
+
+
+
+// let arrayOfstudents = [];
+
+// arrayOfstudents.push("Zulfugar");
+// arrayOfstudents.push("Serxan");
+// arrayOfstudents.push("Emil");
+
+// console.log(arrayOfstudents.length);
+
+// let yaxciOglan = arrayOfstudents.pop();
+// let yaxciOglan2 = arrayOfstudents.shift();
+// console.log(yaxciOglan2,"hehhehe");
+// arrayOfstudents.unshift("Deputat");
+// console.log(arrayOfstudents);
 
 
 
--- js/assets/lessons/lesson-13.js
+++ js/assets/lessons/lesson-13.js
@@ -5,9 +5,66 @@ Başqa bir qrup yoldaşının adını massivin sonuna əlavə edin;
 İlk adı silin;
 Massivi konsola çıxarın.
  */
-const classMates = [];
-classMates.push("Dingo-Bingo");
-classMates.push('Zulfugar');
-classMates.push('Qazanfar');
-classMates.shift();
-console.log(classMates);
+// const classMates = [];
+// classMates.push("Dingo-Bingo");
+// classMates.push('Zulfugar');
+// classMates.push('Qazanfar');
+// classMates.shift();
+// console.log(classMates);
+
+
+/**
+ * Verilən mətn tip dəyərdəki vergülləri nöqtəli vergüllə əvəz edin.
+'32, 31, 34, 36, 31' mətni verilmişdir;
+İçindəki vergülləri nöqtəli vergüllə əvəz edin.
+ */
+
+// let numbers = "32, 31, 34, 36, 31";
+// numbers = numbers.split(",").join(";");
+
+
+/**
+ * 10 ədəddən ibarət massiv yaradın.
+   İlk 2 ədədi dəyişənlərə yazın və qalanlardan yeni massiv yaradın.
+ */
+
+// const numberParts = [11, 34, 566, 86, 445, 665, 7, 878, 997, 35, 23];
+
+// let [a, b, ...others] = numberParts;
+
+// console.log(a,b,others)
+
+/**
+ * 
+ * 10 rəqəmdən ibarət massiv yaradın.
+Massiv deyil, arqumentlər qəbul edən Math.max funksiyasından istifadə edərək onların arasında maksimumu tapın.
+spread-operatorundan istifadə edərək massiv arqumentlərini funksiyaya ötürün.
+ */
+
+
+
+// const numberParts = [11, 34, 566, 86, 445, 665, 7, 878, 997, 35, 23];
+
+// console.log(Math.max(...numberParts));
+
+
+/**
+ * 
+ * Massivin bütün elementlərinin dəyərin ikiqat artırın.
+5 rəqəmdən ibarət massiv yaradın.
+Orijinal massivin surətini çıxarın və yeni massivin bütün elementlərini ikiqat artırın.
+Orijinal massivin dəyişmədiyini yoxlayın.
+ */
+
+// const original = [11, 34, 566, 86, 445];
+
+// const copy = [...original];
+
+// for (let item in copy) {
+//     copy[item] = copy[item]*2
+// }
+
+// console.log('Original--',original);
+
+// console.log('Copy ---',copy)
+
